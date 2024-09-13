80098115e8fdd5791cf30b47f2d364473ffd1cc3
useifabdelhady <yoazazy55@gmail.com>
2024-09-13 10:52:58 +0300

    Group-Anagrams & Valid-Anagram

---
+++ Group-Anagrams/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace Group_Anagrams;
+public class Solution
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        if (strs.Length == 0)
+        {
+            return new List<IList<string>>();
+        }
+
+        Dictionary<string, List<string>> ansMap = new Dictionary<string, List<string>>();
+        int[] count = new int[26];
+
+        foreach (string s in strs)
+        {
+            Array.Fill(count, 0);
+            foreach (char c in s)
+            {
+                count[c - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++)
+            {
+                sb.Append("#");
+                sb.Append(count[i]);
+            }
+
+            string key = sb.ToString();
+            if (!ansMap.ContainsKey(key))
+            {
+                ansMap[key] = new List<string>();
+            }
+            ansMap[key].Add(s);
+        }
+
+        return new List<IList<string>>(ansMap.Values);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        string[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+        IList<IList<string>> result = solution.GroupAnagrams(strs);
+        foreach (var group in result)
+        {
+            Console.WriteLine(string.Join(", ", group));
+        }
+    }
+}
---
+++ Valid-Anagram/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Valid_Anagram;
+ public class Solution
+{
+  public bool IsAnagram(string s, string t)
+{
+   if(s.Length != t.Length)
+   {
+    return false;
+   }
+   int[] count  = new int[26];
+   for(int i =0; i<s.Length; i++)
+   {
+    count[s[i] - 'a']++;
+    count[t[i] - 'a']--;
+   }
+   for(int i =0; i<26; i++)
+   {
+    if(count[i] != 0)
+    {
+        return false;
+    }
+   }
+   return true;
+}
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        Console.WriteLine(solution.IsAnagram("anagram", "nagaram")); // Output: True
+        Console.WriteLine(solution.IsAnagram("rat", "car")); // Output: False
+    }
+}
